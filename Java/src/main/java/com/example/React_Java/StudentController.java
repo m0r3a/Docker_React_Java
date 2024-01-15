d8fb4976a65a107d5ede4eacf7485f40cc8856f5
m0r3a <buretdan52@gmail.com>
2024-01-15 17:31:59 +0200

    yes

--- Java/src/main/java/com/example/React_Java/StudentController.java
+++ Java/src/main/java/com/example/React_Java/StudentController.java
@@ -7,11 +7,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000") // Apply CORS globally to all endpoints in this controller
+@CrossOrigin(origins = "http://localhost:3000")
 public class StudentController {
 
     @Autowired
-    private StudentService studentService;  // Assuming you have a service to handle data access
+    private StudentService studentService;
 
     @GetMapping("/students")
     public List<Student> getAllStudents() {
